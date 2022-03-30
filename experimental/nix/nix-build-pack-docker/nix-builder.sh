430df56709661291e1fbc7d75884df418d9c93ad
Romeo Stoll <47355990+rstol@users.noreply.github.com>
2022-03-30 22:17:27 +0200

    makefile and shell script works

--- experimental/nix/nix-build-pack-docker/nix-builder.sh
+++ experimental/nix/nix-build-pack-docker/nix-builder.sh
@@ -2,7 +2,21 @@
 mkdir -p out
 nix-build $1
 # if [ -h result/etc ]; then echo Error: Build resulted /etc as symlink && exit 1; fi
-closurePath=$(nix-store -qR result)
-cp $(nix-store -qR result) ./out
-tarName=$(nix-store -qR result | sed -ne 's|^/nix/store/\(.*\)|\1|p')
-ln -sf "out/"$tarName result
+if [ -L "result" ]; then
+  tarName=$(nix-store -qR result | sed -ne 's|^/nix/store/\(.*\)|\1|p')
+
+  if [ "$tarName" ]; then
+    echo $tarName
+  else
+    exit 1
+  fi
+  if [ ! -f "out/$tarName" ]; then
+    cp $(nix-store -qR result) ./out/
+  fi
+  if [ -f "out/$tarName" ]; then
+    rm outlink
+    ln -s "out/$tarName" outlink
+  else
+    echo "symlink could not be created, target does not exist"
+  fi
+fi
