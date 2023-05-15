5f0b6f14cc22c91ac752d095fe51102674a74648
Romeo Stoll <47355990+rstol@users.noreply.github.com>
2023-05-15 13:49:25 +0200

    remove thesis template, deleted files, and experimental folder

--- prototypes/nix-docker-builder/nix/hash-files.nix
+++
@@ -1,5 +0,0 @@
-with builtins;
-files:
-  if isList files
-  then hashString "md5" (foldl' (x: y: x + (hashFile "md5" y)) "" (sort lessThan files))
-  else hashFile "md5" files
--- prototypes/nix-docker-shell-repl/workdir/hello_world.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-if __name__ == '__main__':
-  x = np.arange(15, dtype=np.int64).reshape(3, 5)
-  x[1:, ::2] = -99
-  print("Testing numpy")
-  print(x)
