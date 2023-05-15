a4fab3aa3677230a57fe9ccbb8f088d545e6e2e2
Romeo Stoll <47355990+rstol@users.noreply.github.com>
2023-05-15 13:05:29 +0200

    Delete experimental directory

--- experimental/nix/python_mach-nix_ex/local.nix
+++
@@ -1,7 +0,0 @@
-with (import <nixpkgs> {});
-{
-  inputs = [ curl python39Packages.numpy nodejs-17_x];
-  hooks = ''
-    export FOO=bar
-  '';
-}
--- experimental/nix/rust_nodejs.nix
+++
@@ -1,5 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-pkgs.mkShell {
-    nativeBuildInputs = [ pkgs.rustc pkgs.cargo pkgs.nodejs-16_x ];
-}
